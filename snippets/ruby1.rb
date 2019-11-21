bb4b7714031878ee29728bbb2c246ab5a5bea2a9
singingcactus <bechma02@gmail.com>
2019-11-20 23:51:25 -0500

    blog 1 complete, I hope

--- snippets/ruby1.rb
+++ snippets/ruby1.rb
@@ -2,15 +2,23 @@ module MyModule
     def clarity
         "This code is in the clarity method of MyModule"
     end
+    
+    def num
+        @num
+    end
+
+    def add_one
+        self.num + 1
+    end
 end
 
-
 class MyClass
-    extend MyModule
+    prepend MyModule
 
-    def initialize(name)
-        @name = name
+    def initialize(num)
+        @num = num
     end
+
 
     def clarity
         "this code is in the clarity instance method of MyClass"
@@ -18,8 +26,12 @@ class MyClass
 
 end
 
-instance1 = MyClass.new(instance1)
+instance1 = MyClass.new(1)
 
-puts MyClass.clarity
+# puts MyClass.clarity
 puts instance1.clarity
+puts instance1.add_one
+puts MyClass.ancestors.first
+puts Math.sqrt(4)
+puts Math::PI
 
