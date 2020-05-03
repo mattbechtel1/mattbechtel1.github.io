ce87c373ec1ee01851b08a1b2b6a554fd657b824
Matt Bechtel <bechma02@gmail.com>
2020-05-03 17:56:00 -0400

    blog complete

--- snippets/add_cascade_delete.rb
+++
@@ -1,7 +0,0 @@
-# db/migrate/AddForeignKeyToBeds.rb
-class ChangeForeignKeyToBeds < ActiveRecord::Migration[6.0]
-    def change
-        remove_foreign_key :beds, :field
-        add_foreign_key :field, :beds, on_delete: :cascade
-    end
-end
